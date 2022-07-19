5504848b1054deeb16787347933c7de91ed1e2fd
Shion Ichikawa <shion1305@gmail.com>
2022-07-20 06:14:23 +0900

    🎉 Initial Commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module discordLinkShortener
+
+go 1.18
+
+require (
+	github.com/bwmarrin/discordgo v0.25.0 // indirect
+	github.com/dyatlov/go-opengraph/opengraph v0.0.0-20220524092352-606d7b1e5f8a // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"golang.org/x/net/html"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+)
+
+func main() {
+	dg, err := discordgo.New("Bot " + os.Getenv("token"))
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+	}
+	dg.AddHandler(onMessageCreate)
+	stopBot := make(chan os.Signal, 1)
+
+	signal.Notify(stopBot, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	<-stopBot
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	d := amazonLinkVerifier(m.Content)
+	fmt.Println(d)
+	if d == nil {
+		return
+	}
+	_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+		Title:       d.Title,
+		Description: d.Url,
+		URL:         d.Url,
+		Type:        discordgo.EmbedTypeLink,
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    m.Author.Username,
+			IconURL: m.Author.AvatarURL(""),
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Amazonリンクが共有されました",
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
+	}
+}
+
+type AmazonUrlInfo struct {
+	Title string
+	Url   string
+}
+
+func amazonLinkVerifier(link string) *AmazonUrlInfo {
+	r, _ := regexp.Compile(`^https://www\.amazon\.co\.jp/[^ ]+/dp/(?P<id>[A-Za-z\d]+).*$`)
+	result := r.FindString(link)
+	fmt.Println("result: ", result)
+	if result == "" {
+		return nil
+	}
+	outLink := r.ReplaceAllString(link, "https://www.amazon.co.jp/dp/${id}")
+	title := getOGP(outLink)
+	fmt.Println("OutLink: ", outLink)
+	return &AmazonUrlInfo{title, outLink}
+}
+
+func getOGP(link string) string {
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", link, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	var title string
+	if t, ok := GetHtmlTitle(resp.Body); ok {
+		title = t
+		fmt.Println(title)
+	} else {
+		println("Fail to get HTML title")
+	}
+	return title
+}
+
+func isTitleElement(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.Data == "title"
+}
+
+func traverse(n *html.Node) (string, bool) {
+	if isTitleElement(n) {
+		return n.FirstChild.Data, true
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result, ok := traverse(c)
+		if ok {
+			return result, ok
+		}
+	}
+
+	return "", false
+}
+
+func GetHtmlTitle(r io.Reader) (string, bool) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		panic("Fail to parse html")
+	}
+	return traverse(doc)
+}
